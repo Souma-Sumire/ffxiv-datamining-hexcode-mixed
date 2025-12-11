214df661ae58883a2b5c17dfd1911064da2917c0
Souma-Sumire <553469159@qq.com>
2025-12-11 20:18:27 +0800

    feat: tc support

--- README.md
+++ README.md
@@ -2,4 +2,34 @@
 
 复刻自 [InfSein/ffxiv-datamining-mixed](https://github.com/InfSein/ffxiv-datamining-mixed)，但输出 hexcode 格式，以供汉化工具使用。
 
-从 国服7.38／国际服7.38 开始提供。
+从 国服7.38／国际服7.38／繁中服7.0 开始提供。
+
+## 本地解包
+
+为了项目结构需要，此项目实际使用的是复刻／修改后的：
+
+- [Souma-Sumire/dumpcsv](https://github.com/Souma-Sumire/dumpcsv)
+- [Souma-Sumire/SaintCoinach-hexcode](https://github.com/Souma-Sumire/SaintCoinach-hexcode)
+
+如果想要从本地的游戏文件中提取出csv文件，请确保：
+
+- 已安装 `.NET 7 运行时` 和 `NodeJS (推荐22.x)`
+
+然后，按照以下步骤操作：
+
+1. 下载或clone此项目；
+2. 在项目目录下打开终端，执行 `npm i` ；
+3. 将项目根目录下的 `config.json.example` 复制一份，重命名为 `config.json`，然后在其中相应的位置填写你国服／国际服／繁中服的游戏目录；
+   > 如果你不需要解包某些服务器的文本，则不需要填写其目录，在后续步骤中不执行对应解包指令即可。
+4. 在终端执行 `npm run update-unpacker` ，更新解包工具；
+5. 按需执行以下解包指令。
+
+   | 解包指令              | 说明                                          |
+   | :------------------- | :------------------------------------------- |
+   | `npm run unpack:chs` | 解包国服文本，输出简体中文的csv到 `chs` 目录下。 |
+   | `npm run unpack:en`  | 解包国际服文本，输出英文的csv到 `en` 目录下。    |
+   | `npm run unpack:ja`  | 解包国际服文本，输出日文的csv到 `ja` 目录下。    |
+   | `npm run unpack:tc`  | 解包繁中服文本，输出繁体中文的csv到 `tc` 目录下。    |
+
+如此即可完成初次解包。
+在初次解包之后，下次及之后的解包可以直接从第4步开始。
--- scripts/unpack.ts
+++ scripts/unpack.ts
@@ -1,50 +1,51 @@
-import fs from "fs"
-import path from "path"
-import { execSync } from "child_process"
-import AppConfig from '../config.json'
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
+import AppConfig from "../config.json";
 
-const availableLangs = [
-  'chs', 'en', 'ja',
-] as const
-type AvailableLang = typeof availableLangs[number]
+const availableLangs = ["chs", "en", "ja", "tc"] as const;
+type AvailableLang = (typeof availableLangs)[number];
 
 const getServer = (lang: AvailableLang) => {
   switch (lang) {
-    case 'chs': return 'chs'
-    default: return 'global'
+    case "chs":
+      return "chs";
+    case "en":
+      return "global";
+    case "ja":
+      return "global";
+    case "tc":
+      return "tc";
+    default:
+      throw new Error("未知的语言参数");
   }
-}
+};
 
 const unpack = (lang: AvailableLang) => {
   if (!lang || !availableLangs.includes(lang)) {
-    console.error(`请提供合法的运行参数。\n示例：\n> ts-node scripts/unpack.ts [${availableLangs.join('|')}]`)
-    process.exit(1)
+    console.error(`请提供合法的运行参数。\n示例：\n> ts-node scripts/unpack.ts [${availableLangs.join("|")}]`);
+    process.exit(1);
   }
 
-  const server = getServer(lang)
-  const gamePath = AppConfig.gamePath[server]
+  const server = getServer(lang);
+  const gamePath = AppConfig.gamePath[server];
   if (!fs.existsSync(gamePath)) {
-    console.error(`未找到 ${lang} 的游戏路径，请先配置 config.json 文件。\n gamePath: ${gamePath}`)
-    process.exit(1)
+    console.error(`未找到 ${lang} 的游戏路径，请先配置 config.json 文件。\n gamePath: ${gamePath}`);
+    process.exit(1);
   }
 
-  const unpackerPath = path.resolve(
-    "tools/unpacker/DumpCsv.exe"
-  )
+  const unpackerPath = path.resolve("tools/unpacker/DumpCsv.exe");
   if (!fs.existsSync(unpackerPath)) {
-    console.error(`未找到 unpacker 执行文件。请运行 package.json 中的 update-unpacker 脚本。\n unpackerPath: ${unpackerPath}`)
-    process.exit(1)
+    console.error(`未找到 unpacker 执行文件。请运行 package.json 中的 update-unpacker 脚本。\n unpackerPath: ${unpackerPath}`);
+    process.exit(1);
   }
 
-  const outputPath = `../../${lang}`
-  fs.mkdirSync(outputPath, { recursive: true })
+  const outputPath = `../../${lang}`;
+  fs.mkdirSync(outputPath, { recursive: true });
 
-  console.log(`开始进行 ${lang} 的解包...`)
-  execSync(
-    `"${unpackerPath}" "${gamePath}" ${lang} rawexd "${outputPath}"`,
-    { stdio: "inherit", cwd: path.dirname(unpackerPath) }
-  )
-}
+  console.log(`开始进行 ${lang} 的解包...`);
+  execSync(`"${unpackerPath}" "${gamePath}" ${lang} rawexd "${outputPath}"`, { stdio: "inherit", cwd: path.dirname(unpackerPath) });
+};
 
-const server = process.argv[2] as AvailableLang
-unpack(server)
+const server = process.argv[2] as AvailableLang;
+unpack(server);
